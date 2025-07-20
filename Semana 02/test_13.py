d12fc5f3640d4e254eb784cff55cdd6827825328
“Anthony <acarrillorj@gmail.com>
2025-07-20 12:59:35 -0500

    inicio de proyecto

---
+++ Semana 02/test_13.py
@@ -0,0 +1,16 @@
+"""
+Operadores comunes
+Operador multiplicación: *
+"""
+
+multiplica_1 = 9 * 2000
+multiplica_2 = 90 * 9.88
+multiplica_3 = 9.55 * 100.40
+
+print(multiplica_1)
+print(multiplica_2)
+print(multiplica_3)
+
+print("___________________________________")
+multiplica_4 = "\nMeses de invierno 2025 Lima Perú"
+print(multiplica_4 * 5)
